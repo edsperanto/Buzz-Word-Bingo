2af14a41b2c55a577a15ac56ff3ebefa8e63f048
Edward Gao <egao@sent.com>
2017-01-04 21:44:15 -1000

    renders index.html from root

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+const fs = require('fs');
 const express = require('express');
 const buzzwords = require('./routes/buzzwords');
 const buzzword = require('./routes/buzzword');
 const reset = require('./routes/reset');
 let app = express();
+let buzzWords = [];
 
+app.use('/buzzwords', buzzwords);
+app.use('/buzzword', buzzword);
+app.use('/reset', reset);
 
+app.get('/', function(req, res, next) {
+	res.type('html');
+	res.status(200);
+	fs.readFile('./index.html', function(err, content) {
+		res.send(content);
+		res.end();
+	});
+});
 
 app.listen(3000, function() {
 	let host = this.address().address;
 	let port = this.address().port;
-
 	console.log(`Server is listening on http://${host}:${port}`);
 });
