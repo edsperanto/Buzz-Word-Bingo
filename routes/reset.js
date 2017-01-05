43d352eac7af918232d498f0725c6175f4e21ae9
Edward Gao <egao@sent.com>
2017-01-04 23:26:49 -1000

    POST to reset works

--- routes/reset.js
+++ routes/reset.js
@@ -2,6 +2,13 @@ const express = require('express');
 const my = require('./../public/my.js');
 let router = express.Router();
 
-
+router.post('/', function(req, res, next) {
+	my.buzzWords = [];
+	my.score = 0;
+	res.type('json');
+	res.status(200);
+	res.send(`{ "success": true }`);
+	res.end();
+});
 
 module.exports = router;
